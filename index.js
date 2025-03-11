29e053a95aedff9374debb45ae16c41bd9b11030
Jean Raphael Barbosa Santos <jrbasan@hotmail.com>
2025-03-11 16:44:02 -0700

    lab done

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+
+const titleCased = () => tutorials.map(words => {
+  //split words
+  //Slice word's 0 char and modify it to be upper cased.
+  //Slice words from char 1 and beyond keeping it the same way it was before being sliced for the first time.
+  //Join everything back together resulting in words that have char 0 upper cased.
+  const titleCasedWords = words.split(" ").map(word => word.slice(0, 1).toUpperCase() + word.slice(1)).join(" ");
+  return titleCasedWords;
+});
+
+//console.log(titleCased(tutorials));
